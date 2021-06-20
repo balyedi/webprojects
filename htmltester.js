1b593f37c773cb5bf2e935f2f4631592c2220509
balyedi <76970911+balyedi@users.noreply.github.com>
2021-06-20 18:47:25 +0300

    Update htmltester.js

--- htmltester.js
+++ htmltester.js
@@ -1,6 +1,6 @@
 function test() {
 const myString = document.getElementsByTagName("TEXTAREA")[0].value;
-const encoded = btoa(myString);
+const encoded = encodeURIComponent(myString);
 //console.log(encoded);                 OCY5JjomOyY8Jj4mPyY=
-document.getElementsByTagName("IFRAME")[0].setAttribute("src", `data:text/html;base64,${encoded}`);
+document.getElementsByTagName("IFRAME")[0].setAttribute("src", `data:text/html;,${encoded}`);
 }
